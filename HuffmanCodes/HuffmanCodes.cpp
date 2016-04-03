36ed95555e7f6b90d8e22e2c3dcc9a758c49c628
wuQAQ <302538257@qq.com>
2016-04-03 19:45:54 +0800

    还需修改字符串拷贝

--- HuffmanCodes/HuffmanCodes.cpp
+++ HuffmanCodes/HuffmanCodes.cpp
@@ -1,196 +1,6 @@
-#if 0
-#include <stdio.h>
-#include <malloc.h>
-
-#define MinPQsize		2
-#define MaxPQsize		63
-#define MinData			-1
-#define HeapStructType	HuffmanTreeNode
-
-struct HeapStruct;
-typedef struct HeapStruct *PriorityQueue;
-
-struct TreeNode;
-typedef struct TreeNode HuffmanTreeNode;
-typedef struct TreeNode *HuffmanTree;
-
-struct InfoNode;
-typedef struct InfoNode *HuffmanInfo;
-
-HuffmanTree Huffman(PriorityQueue H);
-
-//优先队列的基本操作
-int IsEmpty(PriorityQueue H);
-int IsFull(PriorityQueue H);
-PriorityQueue Initialize(int MaxElments);
-HeapStructType DeleteMin(PriorityQueue H);
-PriorityQueue Insert(HeapStructType X, PriorityQueue H);
-
-//定义哈夫曼树的结构体
-struct TreeNode
-{
-	//char Element;
-	int Weight;
-	HuffmanTree Left;
-	HuffmanTree Right;
-};
-
-//定义优先队列的结构体
-struct HeapStruct
-{
-	int Capacity;
-	int Size;
-	HeapStructType *Elements;
-};
-
-struct InfoNode 
-{
-	char Data;
-	int Weight;
-};
-
-int main()
-{
-	PriorityQueue H = NULL;
-	HuffmanTree	  HT = NULL;
-	HuffmanInfo	  HI = NULL;
-
-	HeapStructType *temp = NULL;
-	int HuffmanSize = 0;
-	int *test;
-
-	scanf("%d", &HuffmanSize);
-	HI = (HuffmanInfo)malloc(sizeof(struct InfoNode) * HuffmanSize);
-	temp = (HeapStructType *)malloc(sizeof(struct TreeNode));
-	
-	H = Initialize(HuffmanSize);
-
-	for (int i = 0; i < HuffmanSize; i++)
-	{
-		getchar();
-		scanf("%c %d", &HI[i].Data, &HI[i].Weight);
-		temp->Weight = HI[i].Weight;
-		temp->Right = temp->Left = NULL;
-		H = Insert(*temp, H);
-	}
-
-	printf("HI:\n");
-	for (int i = 0; i < HuffmanSize; i++)
-	{
-		printf("%c %d\n", HI[i].Data, HI[i].Weight);
-	}
-
-	printf("H:\n");
-	for (int i = 0; i < HuffmanSize + 1; i++)
-	{
-		printf("%d\n", H[i].Elements->Weight);
-	}
-	//HT = Huffman(H);
-
-
-	return 0;
-}
-
-//构建哈夫曼树
-HuffmanTree Huffman(PriorityQueue H)
-{
-	int i; 
-	HuffmanTree T;
-
-	for (i = 1; i < H->Size; i++)
-	{
-		T = (HuffmanTree)malloc(sizeof(struct TreeNode));
-		*T->Left = DeleteMin(H);	
-		*T->Right = DeleteMin(H);
-		T->Weight = T->Left->Weight + T->Right->Weight;
-		Insert(*T, H);
-	}
-
-	*T = DeleteMin(H);
-	return T;
-}
-
-//删除优先队列的最小值
-HeapStructType DeleteMin(PriorityQueue H)
-{
-	int i, Child;
-	HeapStructType MinElement, LastElement;
-
-	if (IsEmpty(H))
-		return H->Elements[0];
-
-	MinElement = H->Elements[1];
-	LastElement = H->Elements[H->Size--];
-
-	for (i = 1; i * 2 <= H->Size; i = Child)
-	{
-		Child = i * 2;
-		if (Child != H->Size && H->Elements[Child + 1].Weight < H->Elements[Child].Weight)
-			Child++;
-
-		if (LastElement.Weight > H->Elements[Child].Weight)
-			H->Elements[i] = H->Elements[Child];
-		else
-			break;
-	}
-	H->Elements[i] = LastElement;
-	return MinElement;
-}
-
-//插入到优先队列
-PriorityQueue Insert(HeapStructType X, PriorityQueue H)
-{
-	int i = 0;
-
-	if (IsFull(H))
-		return NULL;
-
-	for (i = ++H->Size; H->Elements[i / 2].Weight > X.Weight; i /= 2)
-		H->Elements[i] = H->Elements[i / 2];
-
-	H->Elements[i] = X;
-
-	return H;
-}
-
-
-int IsFull(PriorityQueue H)
-{
-	return H->Size == H->Capacity;
-}
-
-int IsEmpty(PriorityQueue H)
-{
-	return H->Size == 0;
-}
-
-//优先队列的初始化
-PriorityQueue Initialize(int MaxElments)
-{
-	PriorityQueue H = NULL;
-
-	if (MaxElments < MinPQsize || MaxElments > MaxElments)
-		return NULL;
-
-	H = (PriorityQueue)malloc(sizeof(struct HeapStruct));
-	if (H == NULL)
-		printf("Out of space");
-
-	H->Elements = (HeapStructType *)malloc(sizeof(struct TreeNode) * (MaxElments + 1));
-	if (H->Elements == NULL)
-		printf("Out of space");
-
-	H->Capacity = MaxElments;
-	H->Size = 0;
-	H->Elements[0].Weight = MinData;
-
-	return H;
-}
-
-#endif
-
 #include <malloc.h>
 #include <stdio.h>
+#include <string.h>
 
 struct InfoNode;
 typedef struct InfoNode *HuffmanInfo;
@@ -222,20 +32,33 @@ struct HeapStruct
 	HuffmanTree TreeCell;
 };
 
+
+
 HuffmanTree DeleteMin(PriorityQueue H);
 int IsEmpty(PriorityQueue H);
 PriorityQueue Initialize(int MaxElements);
 int IsFull(PriorityQueue H);
 void Insert(HuffmanTree X, PriorityQueue H);
 HuffmanTree Huffman(PriorityQueue H);
+int GetWPL(HuffmanTree HT, int Deepth);
+int FindWeight(HuffmanInfo HI, char X, int Len);
+int IsPrefix(char Number[][64], int len);
 
 int main()
 {
 	int HuffmanSize = 0;
+	int WPL = 0;
 	HuffmanInfo HI = NULL;
 	PriorityQueue H = NULL;
 	HuffmanTree	HT = NULL;
 	HuffmanTree temp = NULL;
+
+	int CompareGroup = 0;
+	char CompareChar;
+	char Number[64][64];
+	int CompareWPL = 0;
+	int test = 0;
+
 	scanf("%d", &HuffmanSize);
 
 	HI = (HuffmanInfo)malloc(sizeof(struct InfoNode) * HuffmanSize);
@@ -252,21 +75,96 @@ int main()
 		Insert(temp, H);
 	}
 
-	printf("HI:\n");
-	for (int i = 0; i < HuffmanSize; i++)
-	{
-		printf("%c %d\n", HI[i].Data, HI[i].Weight);
-	}
-
-	printf("H:\n");
-	for (int i = 0; i < HuffmanSize; i++)
-	{
-		printf("%d\n", H->TreeCell[i].Weight);
-	}
-
 	HT = Huffman(H);
+	WPL = GetWPL(HT, 0);
+
+	printf("end\n");
+	scanf("%d", &CompareGroup);
+
+	//ComperList = (PNumberChar)malloc(sizeof(struct NumberChar) * CompareGroup);
+	
+	for (int i = 0; i < CompareGroup; i++)
+	{
+		for (int j = 0; j < HuffmanSize; j++) 
+		{
+			getchar();
+			scanf("%c %s", &CompareChar, Number[i]);
+			test = FindWeight(HI, CompareChar, HuffmanSize);
+			CompareWPL += FindWeight(HI, CompareChar, HuffmanSize) * strlen(Number[i]);
+		}
+		if (CompareWPL == WPL)
+		{
+			if (IsPrefix(Number, HuffmanSize))
+				printf("Yes\n");
+			else
+				printf("No\n");
+		}
+		else
+			printf("No\n");
+		CompareWPL = 0;
+	}
+	
 
 	return 0;
+}
+
+int IsPrefix(char Number[][64], int len)
+{
+	char *temp;
+	char ComperTemp[64];
+	for (int j = 0; j < len - 1; j++)
+	{
+		for (int i = 0; i < len - 1 - j; i++)
+		{
+			if (strlen(Number[i]) > strlen(Number[i + 1]))
+			{
+				temp = Number[i];
+				Number[i][64] = Number[i + 1][64];
+				Number[i + 1][64] = *temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < len; i++)
+	{
+		printf("Number:%s\n", Number[i]);
+	}
+
+	for (int i = 0; i < len - 1; i++)
+	{
+		temp = Number[i];
+		for (int j = i + 1; j < len - 1; j++)
+		{
+			strncpy(ComperTemp, temp, strlen(Number[i]));
+			ComperTemp[strlen(Number[i])] = '\0';
+			if (strcmp(temp, ComperTemp) == 0)
+				return 0;
+		}
+
+	}
+	return 1;
+}
+
+int FindWeight(HuffmanInfo HI, char X, int Len)
+{
+	for (int i = 0; i < Len; i++)
+	{
+		if (HI[i].Data == X)
+			return HI[i].Weight;
+	}
+}
+
+int GetWPL(HuffmanTree HT, int Deepth)
+{
+	if (HT->Left == NULL && HT->Right == NULL)
+	{
+		return (HT->Weight * Deepth);
+	}	
+	else
+	{
+		Deepth++;
+		return(GetWPL(HT->Left, Deepth) + GetWPL(HT->Right, Deepth));
+	}
 }
 
 HuffmanTree Huffman(PriorityQueue H)
@@ -279,19 +177,10 @@ HuffmanTree Huffman(PriorityQueue H)
 	{
 		HT = (HuffmanTree)malloc(sizeof(struct HuffmanNode));
 		HT->Left = DeleteMin(H);
-		printf("HT->Left:%d\n", HT->Left->Weight);
 		HT->Right = DeleteMin(H);
-		printf("HT->Right:%d\n", HT->Right->Weight);
 		HT->Weight = HT->Left->Weight + HT->Right->Weight;
 		Insert(HT, H);
-		printf("H:\n");
-		for (int i = 0; i < j; i++)
-		{
-			printf("%d\n", H->TreeCell[i].Weight);
-		}
-		j--;
 	}
-	//HT = DeleteMin(H);
 	return HT;
 }
 
